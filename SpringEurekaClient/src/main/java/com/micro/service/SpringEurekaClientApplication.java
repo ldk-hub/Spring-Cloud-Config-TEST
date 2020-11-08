087dfe4a8870dd60eb693b569e2e252283c2dfc3
Dongok Lee <orm6711@gmail.com>
2020-11-08 19:56:55 +0900

    클라이언트 업데이트

---
+++ SpringEurekaClient/src/main/java/com/micro/service/SpringEurekaClientApplication.java
@@ -0,0 +1,39 @@
+package com.micro.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SpringEurekaClientApplication {
+	 
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaClientApplication.class, args);
+	}
+
+	 	@Bean
+	    @LoadBalanced
+	    public RestTemplate restTemplate() {
+	        return new RestTemplate();
+	    }
+
+	 	@Autowired
+	     RestTemplate restTemplate;
+	 	
+	 	
+	@RequestMapping(value="/")
+	public String hoem() {
+		return "Eureka Client Application Test2";
+	}
+}
