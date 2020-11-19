1899737baade8044a983a852f6ef81566ec8f710
Dongok Lee <orm6711@gmail.com>
2020-11-19 16:39:49 +0900

    스프링 리본,로드밸런싱 실습코드수정 완료

--- SpirngCloudServer/src/main/java/com/ldk/application/SpirngCloudServerApplication.java
+++ SpirngCloudServer/src/main/java/com/ldk/application/SpirngCloudServerApplication.java
@@ -1,17 +1,60 @@
 package com.ldk.application;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 
-@SpringBootApplication
+@RestController
+public class SpirngCloudServerApplication {
+	
+@Autowired
+//private DiscoveryClient discoveryClient;
+private LoadBalancerClient loadBalancerClient;
+
+	@GetMapping("/eureka/client")
+	public void getEmployee() throws RestClientException, IOException {
+    /*
+    List<ServiceInstance> instances=discoveryClient.getInstances("employee-producer");
+    ServiceInstance serviceInstance=instances.get(0);
+    String baseUrl=serviceInstance.getUri().toString();
+    */
+	    ServiceInstance serviceInstance = loadBalancerClient.choose("employee-producer");
+	    System.out.println("#####service-instance-uri:"+serviceInstance.getUri());
+	    String baseUrl = serviceInstance.getUri().toString();
+	
+	    baseUrl=baseUrl+"/employee";
+	
+	    RestTemplate restTemplate = new RestTemplate();
+	    ResponseEntity<String> response=null;
+	    try{
+	        response=restTemplate.exchange(baseUrl,
+	                HttpMethod.GET, getHeaders(),String.class);
+	    }catch (Exception ex)
+	    {
+	        System.out.println(ex);
+	    }
+	    System.out.println(response.getBody());
+	}
+
+		private static HttpEntity<?> getHeaders() throws IOException {
+		    HttpHeaders headers = new HttpHeaders();
+		    headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
+		    return new HttpEntity<>(headers);
+		}
+}
+/*@SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class SpirngCloudServerApplication {
@@ -36,5 +79,4 @@ public class SpirngCloudServerApplication {
 	@RequestMapping(value="/")
 	public String hoem() {
 		return "Eureka Client Application Test1";
-	}
-}
+	}*/
